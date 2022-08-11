c17081e02a844437ed019384c18b56baa44b3507
Zijian Liu <zijian.liu@rea-group.com>
2022-08-11 23:17:28 +0800

    feat: add button to display

--- src/frontend.py
+++ src/frontend.py
@@ -12,13 +12,18 @@ def display_in_section():
 def display_in_random():
     for index, question in enumerate(question_list):
         question.generate(index)
-    st.write("## 结果")
-    sections.sort(key=lambda section: section.calculate(), reverse=True)
-    for section in sections[:3]:
-        st.write(f"{section.title} Score: {section.calculate()}")
+
+
+def button_to_display():
+    clicked = st.button("Submitted")
+    if clicked:
+        sections.sort(key=lambda section: section.calculate(), reverse=True)
+        for section in sections[:3]:
+            st.write(f"{section.title} Score: {section.calculate()}")
 
 
 st.title('荣格的十二人格原型测验')
 
 # display_in_section()
 display_in_random()
+button_to_display()
